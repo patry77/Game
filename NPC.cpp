73cfa5759f64e68245022325fa8412af2de3528f
MTX <96997652+MTX77@users.noreply.github.com>
2022-01-04 16:45:00 +0100

    npc222

--- NPC.cpp
+++ NPC.cpp
@@ -13,7 +13,7 @@ NPC::NPC(Texture *texture){
     body_npc.setPosition(pos_x, pos_y);
     body_npc.setTexture(texture);
 }
-NPC::NPC(Texture *texture, float pos_x, float pos_x){
+NPC::NPC(Texture *texture, float pos_x, float pos_y){
     body_npc.setSize(Vector2f(64.0f, 64.0f));
     body_npc.setOrigin(body_npc.getSize() / 2.0f);
     body_npc.setPosition(pos_x, pos_y);
